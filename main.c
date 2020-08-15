f33d21e9bc037d1e5186e58f4912631df2083220
LI Xiaogang <63132178+lixiaogang5@users.noreply.github.com>
2020-08-15 20:45:44 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,43 @@
+/* ***********************************************************************************
+ * @Function	: 																	 *
+ * @Version		: 1.0.0																 *
+ * @Author		: lixiaogang5														 *
+ * @Date		: 2020-08-12														 *
+ * @Contact 	: lxiaogang5@gmail.com												 *
+ * @Company		: HIKVISION                                                          *
+ * ***********************************************************************************
+ */
+
+#include <string.h>
+#include <stdio.h>
+#include "LWLogc.h"
+
+//extern LWLogcConf logConfigure;
+#define LWLOG_INFO(x) LwlogcMessage(logConfigure.logLevel, __LINE__, __FUNCTION__, LwlogcFormatLogMessage x)
+
+
+int main(int argc, char **argv)
+{
+	#if 1
+	if(1 != LwlogcInit("lwlog.properties"))
+	{
+		fputs("lwlog_init failed.", stderr);
+		sleep(1);
+	}
+
+	
+
+	for(;;) ;//LWLOG_INFO(("lwlogc_message start TEST"));
+	#else
+
+	//FILE *fp = fopen("/data1/lixiaogang5/work/testCode/08_11/LWLogc/test/kms.log", "a+");
+	FILE *fp = fopen("lwlog.properties", "a+");
+	if(NULL == fp) {
+		fputs("fopen failed. ", stderr);
+	}
+	fputs("fopen lwlog.properties succeed.", stdout);
+	#endif 
+	return 0;
+}
+
+
