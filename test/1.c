50a4253ab2ae5bbc0e1225ba53a26fd1ae6ef251
LI Xiaogang <63132178+lixiaogang5@users.noreply.github.com>
2020-08-17 01:36:03 +0800

    Create 1.c

---
+++ test/1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include "LWlogc.h"
+
+
+
+int main()
+{
+	char *loc = "/home/lixiaogang5/Code/LWLogc/test/666.log";
+	int len = 0;
+	len = LwlogcCouNum2SizeOfFiles(loc);
+	printf("@len:%d\n", len);
+
+	LwlogcGetLogsNum(logConfigure.fp);
+
+	return 0;
+}
