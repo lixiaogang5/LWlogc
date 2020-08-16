666a0089463e1b302731d451578099defcb83d4c
LI Xiaogang <63132178+lixiaogang5@users.noreply.github.com>
2020-08-17 01:35:44 +0800

    Update LWlogc.c
    [ADD]
    1. 当前LWlogc日志服务支持进程启动后，往最新的log文件中去写数据；
    2. 支持当log文件大小超过预期后，重新创建一个新的文件；
    3. 动态修改文件打印级别待完善；
    4. log文件数量控制待完善。

--- LWlogc.c
+++ LWlogc.c
@@ -22,22 +22,16 @@
 #include <dirent.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#include "LWLogc.h"
+#include "LWlogc.h"
 
 
 #define LWLOG_STREAM LwlogcGetNowStream()
 static FILE *gStream = 0;
-
-typedef enum {
-	false,
-	true,
-}bool;
-
 static const char *lwlogcLeverStr[] = {"LW_ERROR", "LW_WARN", "LW_INFO", "LW_DEBUG"};
 LWLogcConf logConfigure;
 bool bInit = false;
+static long long perLogSize = 0;
 pthread_attr_t attr; //可采用静态初始化方式.
-
 
 void LwlogcSetStream(const FILE *stream)
 {
@@ -71,9 +65,7 @@ const char *LwlogcTimeNow(char *time_buf)
     time_t time_now_sec = 0;
     size_t len = 0;
 
-	memset(&tv, 0x00, sizeof(tv));
-//	memset(&lt, 00x0, sizeof(lt));
-    
+	memset(&tv, 0x00, sizeof(tv));    
     gettimeofday(&tv,0);
     time_now_sec = tv.tv_sec;
     assert(localtime_r(&time_now_sec, &lt));
@@ -107,11 +99,9 @@ void LwlogcVersion(char *dst)
 
 void LwlogcMessage(LWLogcLevel curLevel, int line, const char *funcName, const char *message)
 {
-	#if defined(NDEBUG)
-	#else
-	assert(funcName && message);
-	#endif 
-
+	assert(message);
+	assert(funcName);
+	int len = 0;
 	char time_buf[TIME_NOW_BUF_SIZE] = {0};
 	LwlogcTimeNow(time_buf);
 	#if defined(POSIX_THREAD)
@@ -122,24 +112,53 @@ void LwlogcMessage(LWLogcLevel curLevel, int line, const char *funcName, const c
 	 * format: 2020-08-12 星期2 17:48:31,982 LW_INFO funcName[line]: other param.
 	 * 参考log4cxx的日志打印风格, lwlogc中增加进程pid and 线程id的打印.
 	 */
-	fprintf(LWLOG_STREAM, "%s %s (%d:%d) %s[%d]: %s\n", time_buf, lwlogcLeverStr[curLevel], 
+	len = fprintf(LWLOG_STREAM, "%s %s (%d:%lu) %s[%d]: %s\n", time_buf, lwlogcLeverStr[curLevel], 
 		pid, pthread_self(), funcName, line, message);
 	#else
-	fprintf(LWLOG_STREAM, "%s %s %s[%d]: %s\n", time_buf, lwlogcLeverStr[curLevel], funcName, line, message);
+	len = fprintf(LWLOG_STREAM, "%s %s %s[%d]: %s\n", time_buf, lwlogcLeverStr[curLevel], funcName, line, message);
 	#endif 
 	
 	fflush(LWLOG_STREAM);
+	if(message) free((void*)message), message = NULL;
+
+	perLogSize += len;
+	if(perLogSize/CONVERT_BYTES_TO_MEGA >= logConfigure.maxFileSize) {
+		perLogSize = 0;
+
+		//关闭旧文件句柄.
+		FILE *fp = LwlogcGetNowStream();
+		fclose(fp);
+
+		//获取最新的文件句柄
+		char arr[128] = {0};
+		size_t len = sizeof(arr);
+		if(true != LWlogcNewestLogFile(arr, &len)){
+			fputs("LWlogcNewestLogFile failed.", stderr);
+			LwlogcSetStream(stdout);
+		}
+		fp = fopen(arr, "a+");
+		if(!fp) {
+			perror("fopen");
+			LwlogcSetStream(stdout);
+			return;
+		}
+
+		LwlogcSetStream(fp);
+		if(LwlogcGetLogsNum(logConfigure.fp) >= logConfigure.maxBackupIndex) LwlogcDeleteOldLogFile();
+	}
+	
 	return;
 }
 
 
 const char *LwlogcFormatLogMessage(const char * format, ...)
 {
-	int len = 0;
 	va_list va;
-	//char *buf_msg = (char *)realloc(1, (TIME_NOW_BUF_SIZE * 2));
-	//if(!buf_msg) perror("realloc"), return "realloc failed.";
-	char buf_msg[TIME_NOW_BUF_SIZE] = {0};
+	char *buf_msg = (char *)calloc(NUM_OF_ELEMENTS, TIME_NOW_BUF_SIZE);
+	if(!buf_msg){
+		perror("calloc");
+		return "calloc memory failed.";
+	}
 	
 	va_start(va, format);
 	/*
@@ -147,8 +166,7 @@ const char *LwlogcFormatLogMessage(const char * format, ...)
      * respectively, except that they are called with a va_list instead of a variable number of arguments.   These  functions  do  not  call  the
      * va_end macro.  Because they invoke the va_arg macro, the value of ap is undefined after the call.  See stdarg(3)
 	 */
-	len = vsnprintf(buf_msg, TIME_NOW_BUF_SIZE - 1, format, va);
-	printf("len: %d\n", len);
+	vsnprintf(buf_msg, TIME_NOW_BUF_SIZE - 1, format, va);
 	va_end(va);
 	return buf_msg;
 }
@@ -161,28 +179,25 @@ int LwlogcInit(const char *configFile)
 		return false;
 	}
 
-	pthread_mutex_init(&logConfigure.mutex, NULL);
-	
 	logConfigure.filePath = NULL;
 	logConfigure.convPattern = NULL;
 	logConfigure.maxBackupIndex = 0;
 	logConfigure.append = 0;
 	logConfigure.maxFileSize = 0;
 	logConfigure.logLevel = LW_INFO;
-	//logConfigure.fp = NULL;
+	logConfigure.fp = NULL;
+	pthread_mutex_init(&logConfigure.mutex, NULL);
 
 	FILE *fp = fopen(configFile, "r");
 	if(!fp) {
-		fputs("fopen configFile failed.", stderr);
+		perror("fopen");
 		fclose(fp);
 		return false;
 	}
 	
 	pthread_t tid;
-
 	pthread_attr_init(&attr);
 	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
-	
 	if( 0 != pthread_create(&tid, &attr, LwlogcReadConf, (void*)fp)) {
 		fputs("pthread_create failed.", stderr);
 		pthread_attr_destroy(&attr);
@@ -192,19 +207,26 @@ int LwlogcInit(const char *configFile)
 
 	//打开写日志文件.
 	while(!bInit) sleep(1);
+	if(false == LwlogcCouNum2SizeOfFiles(logConfigure.filePath)) {
+		fputs("LwlogcCouNum2SizeOfFiles err.", stderr);
+		pthread_attr_destroy(&attr);
+		return false;
+	}
 
-	//{"LW_ERROR", "LW_WARN", "LW_INFO", "LW_DEBUG"};
-	printf("filePath: %s, %d, %d, %d\n", logConfigure.filePath, logConfigure.logLevel, logConfigure.maxFileSize, logConfigure.maxBackupIndex);
+	char buf[128] = {0};
+	size_t buf_len = sizeof(buf);
+	if(false == LWlogcNewestLogFile(buf, &buf_len)) {
+		fputs("LWlogcNewestLogFile failed.", stderr);
+		pthread_attr_destroy(&attr);
+		return false;
+	}
 
-	pthread_mutex_lock(&logConfigure.mutex);
-	FILE *fpW = fopen(/*"/home/000/666.log"*/logConfigure.filePath, "w+");
-	if(!fpW) {
+	FILE *fpN = fopen(buf, "a+");
+	if(!fpN) {
 		perror("fopen");
 		return false;
 	}
-	pthread_mutex_unlock(&logConfigure.mutex);
-
-	LwlogcSetStream(fpW);
+	LwlogcSetStream(fpN);
 	return true;
 }
 
@@ -215,7 +237,7 @@ void* LwlogcReadConf(void *pFd)
 {
 	if(!pFd) {
 		fputs("lwlogc_read_conf param is null.", stderr);
-		return NULL;
+		return (void*)false;
 	}
 
 	/*************************************************************************************************
@@ -232,21 +254,14 @@ void* LwlogcReadConf(void *pFd)
 	
 	for(; ; )
 	{
-		memset(buf_conf, 0x00, TIME_NOW_BUF_SIZE);
 		pthread_mutex_lock(&logConfigure.mutex);
-		for(; fgets(buf_conf, TIME_NOW_BUF_SIZE, fp) != NULL; memset(buf_conf, 0x00, sizeof(buf_conf))) //really read the data: size - 1
+		for(; fgets(buf_conf, TIME_NOW_BUF_SIZE, fp) != NULL; ) //really read the data: size - 1
 		{
 			char *p = NULL;
-			if('#' == buf_conf[0] || isspace(buf_conf[0])) continue;
+			if('#' == buf_conf[0] || isspace(buf_conf[0]) || isdigit(buf_conf[0])) continue;
 			
-			/* #if defined(_GNU_SOURCE) strcasestr
-			 * The strstr() function conforms to C89 and C99.  The strcasestr() function is a nonstandard extension.
-			 * 当在Getting SMBIOS data from sysfs.
-			 * 	SMBIOS 2.7 present
-			 * 该函数的使用出现问题.
-			 */
-			 
-			if((p = strstr(buf_conf, "File"))) {
+			/* strcasestr() function is a nonstandard extension.*/
+			if((p = strstr(buf_conf, "lwlog.File"))) {
 				//while(*(++p) != '/');
 				char buf[512] = {0};
 				int len = 0;
@@ -255,15 +270,14 @@ void* LwlogcReadConf(void *pFd)
 				   || 0 == len
 				   || -1 == len) {
 					fputs("sscanf conf file failed.", stderr);
-					return false;
+					return (void*)false;
 				}
 
 				len = strlen(buf_conf) - strlen(p); //忽略的字节数.
-				
-				logConfigure.filePath = (char*)calloc(1, (strlen(buf_conf) - len - strlen(buf)) + 1);
+				logConfigure.filePath = (char*)calloc(NUM_OF_ELEMENTS, (strlen(buf_conf) - len - strlen(buf)) + 1);
 				if(!logConfigure.filePath) {
 					perror("realloc");
-					return false;
+					return (void*)false;
 				}
 				
 				strncpy(logConfigure.filePath, buf_conf + len + strlen(buf), (strlen(buf_conf) - len - strlen(buf)));
@@ -272,7 +286,7 @@ void* LwlogcReadConf(void *pFd)
 				while(0 == isdigit(*++p));
 				logConfigure.maxBackupIndex = atoi(p);
 				
-			} else if((p = strstr(buf_conf, "MaxFileSize"))) {
+			} else if((p = strstr(buf_conf, "lwlog.MaxFileSize"))) {
 				while(0 == isdigit(*++p));
 				logConfigure.maxFileSize = atoi(p);
 				
@@ -311,25 +325,21 @@ void* LwlogcReadConf(void *pFd)
 				default: {
 					fputs("lwlog.Threshold params err.", stderr);
 					fclose(fp);
-					return false;
+					return (void*)false;
 				}
 				}
 			}
 		}
 
 		bInit = true;
-		// file			level	maxsize maxindex
-		// "",			 0, 	0, 		66
-		// 666.log		 0, 	1024, 	66
-		printf("@@@@file:%s, level:%d, maxsize:%d, maxindex:%d\n", logConfigure.filePath, 
-			logConfigure.logLevel, logConfigure.maxFileSize, logConfigure.maxBackupIndex);
+		rewind(fp);
 		pthread_mutex_unlock(&logConfigure.mutex);
 		sleep(3);
 	}
 
 	pthread_attr_destroy(&attr);
 	fclose(fp);
-	return true;
+	return (void*)true;
 }
 
 
@@ -342,11 +352,11 @@ int LwlogcCouNum2SizeOfFiles(const char *logFilesPath)
 	}
 
 	//获取log文件的路径name
-	char *p = logFilesPath;
-	char *q = p + strlen(p);
+	const char *p = logFilesPath;
+	const char *q = p + strlen(p);
 	while(*--q != '/');
 	char buf[512] = {0};
-	strncpy(buf, q, strlen(p) - strlen(q) + 1/*最后一个'/'*/);
+	strncpy(buf, p, strlen(p) - strlen(q) + 1/*最后一个'/'*/);
 
 	//log4cxx的log文件格式规则: fileName.log, fileName.log1, fileName.log2, 也采取此方式.
 
@@ -365,14 +375,32 @@ int LwlogcCouNum2SizeOfFiles(const char *logFilesPath)
 	 * readdir仅有一个ERRORS, 即EBADF(Invalid directory stream descriptor dirp.)
 	 * 因此, 可省去重置errno操作.
 	 */
+	logConfigure.fp = (struct LwlogcInfoPerFile *)calloc(NUM_OF_ELEMENTS, sizeof(struct LwlogcInfoPerFile));
+	if(!logConfigure.fp) {
+		perror("calloc");
+		return false;
+	}
 
-	struct LwlogcInfoPerFile perFileInfo;
+	logConfigure.fp->pre = logConfigure.fp->next = NULL;
+	logConfigure.fp->fileName = NULL;
+	logConfigure.fp->fileIndex = -1;
+	logConfigure.fp->fileSize = 0;
+	struct LwlogcInfoPerFile *pHead = NULL;
 	
 	while(rddir = readdir(dir)) {
 		if(0 == strncmp(rddir->d_name, ".", 1) 
 		  || 0 == strncmp(rddir->d_name, "..", 2)) continue;
 
 		if(DT_REG != rddir->d_type) continue;
+
+		//可能当前目录下有其他文件.
+		char *str = NULL;
+		str = strstr(q, ".");
+		char fn[50] = {0};
+		strncpy(fn, q + NUM_OF_ELEMENTS, strlen(q) - strlen(str) - NUM_OF_ELEMENTS);
+		if(DT_REG == rddir->d_type 
+		   && (!strstr(rddir->d_name, "log")
+		   || !strstr(rddir->d_name, fn))) continue;
 		/*
 		 * 如果文件属性是目录, 则也忽略. 只检测文件(仅d_name和d_info是POSIX标准)
 		 * 其他为某些Linux所特有, 会降低代码移植性.
@@ -382,21 +410,153 @@ int LwlogcCouNum2SizeOfFiles(const char *logFilesPath)
          * DT_FIFO: named pipe (FIFO).   DT_UNKNOWN: file type is unknown.
 		 */
 
-		char *f = q;
-		f++;
-		if(NULL == strstr(rddir->d_name, f)) continue;
+		//char *f = q;
+		//f++;
+		//if(NULL == strstr(rddir->d_name, f)) continue;
 		
 		struct stat st;
 		memset(&st, 0x00, sizeof(st));
-		//LwlogcInfoPerFile 
-
 		if(0 == stat(rddir->d_name, &st)) {
-			//st.st_size;
-		
+			struct LwlogcInfoPerFile  *pFile = (struct LwlogcInfoPerFile*)calloc(NUM_OF_ELEMENTS, sizeof(struct LwlogcInfoPerFile));
+			if(!pFile) {
+				perror("calloc");
+				return false;
+			}
+			
+			pFile->fileName = (char*)calloc(NUM_OF_ELEMENTS, strlen(rddir->d_name) +    1);
+			if(!pFile->fileName) {
+				perror("calloc");
+				return false;
+			}
+
+			strncpy(pFile->fileName, rddir->d_name, strlen(rddir->d_name));
+			pFile->fileSize = st.st_size;
+
+			char *t = NULL;
+			t = pFile->fileName;
+			while(0 == isdigit(*++t));
+			*t == '\0' ? pFile->fileIndex = 0 : (pFile->fileIndex = atoi(t));
+
+			pHead = logConfigure.fp;
+			while(pHead->next) pHead = pHead->next;
+			pFile->next = pHead->next;
+			pFile->pre = pHead;
+			if(pHead->next) pHead->next->pre = pFile;
+			pHead->next = pFile;
 		}
 	}
 
 	closedir(dir);
+	return true;
+}
+
+
+int LwlogcGetLogsNum(const struct LwlogcInfoPerFile *pHead)
+{
+	if(!pHead) {
+		fputs("LwlogcGetLogsNum null pointer.", stderr);
+		return -1;
+	}
+
+	int num = 0;
+	const struct LwlogcInfoPerFile *p = pHead;
+	while(p->next) num++, p = p->next;
+
+	return num;
+}
+
+
+
+int LwlogcDeleteOldLogFile()
+{
+	int minIndex = 0;
+	struct LwlogcInfoPerFile *pHead = logConfigure.fp;
+	struct LwlogcInfoPerFile *pT = NULL;
+	while(pHead->next) pHead = pHead->next, (pHead->fileIndex < minIndex) ? minIndex = pHead->fileIndex : minIndex;
+	while(pHead->pre) {
+		if(minIndex == pHead->fileIndex) {
+			//删除旧log文件.
+			char buf[128] = {0};
+			strncpy(buf, pHead->fileName, sizeof(buf));
+			if(0 != unlink(buf)) {
+				perror("unlink");
+				return false;
+			}
+
+			if(!pHead->next) {
+				//last node.
+				pT = pHead->pre;
+				pT->next = NULL;
+			}else {
+				pT = pHead->pre;
+				pT->next = pHead->next;
+				pHead->next->pre = pT;
+			}
+
+			free(pHead->fileName);
+			pHead->fileName = NULL;
+			free(pHead);
+			pHead = NULL;
+		}
+
+		pHead = pHead->pre;
+	}
+
+	return true;
+}
+
+
+int LWlogcNewestLogFile(char *pLogBuf, const size_t *bufSize)
+{
+	int maxIndex = 0;
+	struct LwlogcInfoPerFile *pHead = logConfigure.fp;
+	struct LwlogcInfoPerFile *pT = NULL;
+	while(pHead->next)  pHead = pHead->next, (pHead->fileIndex > maxIndex) ? maxIndex = pHead->fileIndex : maxIndex;
+	while(pHead->pre) {
+		if(maxIndex == pHead->fileIndex) {
+			//若该log文件已经超过用户指定预期的大小, 则新创建log文件.
+			if(pHead->fileSize/CONVERT_BYTES_TO_MEGA >= logConfigure.maxFileSize) {
+
+				char buf[128] = {0};
+				char arr[56] = {0};
+				const char *t = pHead->fileName;
+				while('.' != *(++t));
+				strncpy(arr, pHead->fileName, strlen(pHead->fileName) - strlen(t));
+				snprintf(buf, sizeof(buf), "%s.log%d", arr, ++maxIndex);
+				memcpy(pLogBuf, buf, *bufSize);
+				//同时判断log文件数量是否超过用户指定数量.
+				if(LwlogcGetLogsNum(logConfigure.fp) > logConfigure.maxBackupIndex){
+					//删除最旧的log文件.
+					if(false == LwlogcDeleteOldLogFile()) {
+						fputs("LwlogcDeleteOldLog err.", stderr);
+						return false;
+					}
+				}
+			}
+			
+			strncpy(pLogBuf, pHead->fileName, *bufSize); 
+			return true;
+		}
+
+		pHead = pHead->pre;
+	}
+	
+	if(!pHead && 0 == strlen(pLogBuf)) return false;
+	return true;
+}
+
+
+int LwlogcCreateNewLogs(const int *pIndex)
+{
+	if(!pIndex) {
+		fputs("LwlogcCreateNewLogs parameter is empty.", stderr);
+		LwlogcSetStream(stdout);
+		return false;
+	}
+
+	char buf[128] = {0};
+	const struct LwlogcInfoPerFile *pNewestLog = NULL;
+	
 	return true;
 }
 
